c0a360878fa422d3ed19ba96e095e919831bc133
RomainC75 <rom.chenard@gmail.com>
2023-01-17 10:33:15 +0100

    kata3

---
+++ React/D2/kata3.js
@@ -0,0 +1,21 @@
+function isRubyComing(list) {
+    for(let i=0 ; i<list.length ; i++){
+      if(list[i].language==='Ruby'){
+        return true
+      }
+    }
+    return false
+  }
+
+
+  function isRubyComing(list) {
+    return list.find(el=>el.language==='Ruby') ? true : false
+  }
+
+  function isRubyComing(list) {
+    return list.filter(el=>el.language==='Ruby').length>0 ? true : false
+  }
+
+  function isRubyComing(list) {
+    return list.some(el=>el.language==='Ruby')
+  }
