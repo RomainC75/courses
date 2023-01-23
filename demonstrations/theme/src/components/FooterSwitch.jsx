bc098aa433456cf7c82a6c99f3d01f06725b4251
RomainC75 <rom.chenard@gmail.com>
2023-01-23 17:32:11 +0100

    reac-training

--- demonstrations/theme/src/components/FooterSwitch.jsx
+++ demonstrations/theme/src/components/FooterSwitch.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FooterSwitch = ({setTheme}) => {
   const toggleTheme = () => {
-    setTheme( (theme)=>!theme);
+    setTheme( (theme)=> ! theme);
   };
 
   return (
